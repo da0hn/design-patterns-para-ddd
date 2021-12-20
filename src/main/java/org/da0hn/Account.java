84cb4809867e629cd100a25793e3e23506bc5b31
da0hn <gabrielhondacba@gmail.com>
2021-12-19 21:44:04 -0400

    feat: adiciona operações de `débito` e `crédito` através da `transação`

--- src/main/java/org/da0hn/Account.java
+++ src/main/java/org/da0hn/Account.java
@@ -1,11 +1,18 @@
 package org.da0hn;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
+
 public class Account {
   private final String bank;
   private final String branch;
   private final String account;
   private final String document;
-  private int balance;
+  private final List<Transaction> transactions;
+  private final int balance;
 
   public Account(final String bank, final String branch, final String account, final String document) {
     this.bank = bank;
@@ -13,17 +20,24 @@ public class Account {
     this.account = account;
     this.document = document;
     this.balance = 0;
+    this.transactions = new ArrayList<>();
   }
 
-  public void credit(final int amount) {
-    this.balance += amount;
+  public void credit(final double amount) {
+    this.transactions.add(new Transaction("credit", amount));
   }
 
-  public int getBalance() {
-    return this.balance;
+  public double getBalance() {
+    final var transactionsByType = this.transactions.stream()
+      .collect(groupingBy(
+        Transaction::type,
+        summingDouble(Transaction::amount)
+      ));
+
+    return transactionsByType.getOrDefault("credit", 0.0) - transactionsByType.getOrDefault("debit", 0.0);
   }
 
-  public void debit(final int amount) {
-    this.balance -= amount;
+  public void debit(final double amount) {
+    this.transactions.add(new Transaction("debit", amount));
   }
 }
---
+++ src/main/java/org/da0hn/Transaction.java
@@ -0,0 +1,4 @@
+package org.da0hn;
+
+public record Transaction(String type, Double amount) {
+}
